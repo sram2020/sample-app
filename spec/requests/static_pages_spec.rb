51a10a3a95e4e89d13c4ed0d4d1eae0de324bd39
Ramkumar <ramkumar.s@freshdesk.com>
2014-09-10 16:32:48 +0530

    Add a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,24 @@ require 'spec_helper'
 
 describe "Static Pages" do
 	describe "Home page" do
-   		it "should have the content 'Sample App'" do
-			visit '/static_pages/home'
-    		page.should have_content('Sample App')
-    	end
-      it "should have the title 'Home'" do
-          visit '/static_pages/home'
-          page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Home")
-      end
-  	end
-  	describe "Help page" do
+
+    it "should have the h1 'Sample App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Sample App')
+    end
+    
+    it "should have the base title" do
+      visit '/static_pages/home'
+      page.should have_selector('title',
+                        :text => "Ruby on Rails Tutorial Sample App")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  end
+  describe "Help page" do
     	it "should have the content 'Help'" do
       		visit '/static_pages/help'
       		page.should have_content('Help')
@@ -23,9 +30,9 @@ describe "Static Pages" do
         page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | Help")
       end
- 	  end
+ 	end
 
-    describe "About page" do
+  describe "About page" do
       it "should have the content 'About Us'" do
           visit '/static_pages/about'
           page.should have_content('About Us')
@@ -35,5 +42,5 @@ describe "Static Pages" do
           page.should have_selector('title',
                         :text => "Ruby on Rails Tutorial Sample App | About Us")
       end
-    end
+  end
 end
